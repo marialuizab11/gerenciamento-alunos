91478479fd10f330856fd3efe737e25b6e8608cc
marialuizab11 <marialuizabsantos0@gmail.com>
2026-01-28 11:22:27 -0300

    test: implementa testes de integração para UsuarioController"

--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,56 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.service.ServiceUsuario;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveRetornarPaginaLogin() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void deveRetornarPaginaCadastro() throws Exception {
+        this.mockMvc.perform(get("/cadastro"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/cadastro"))
+                .andExpect(model().attributeExists("usuario"));
+    }
+
+    @Test
+    public void cadastrarUsuarioViaController() throws Exception {
+        this.mockMvc.perform(post("/salvarUsuario")
+                .param("email", "controllertest@gmail.com")
+                .param("user", "controllerUser")
+                .param("senha", "123456"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    public void deveEfetuarLogout() throws Exception {
+        this.mockMvc.perform(post("/logout"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"));
+    }
 }
